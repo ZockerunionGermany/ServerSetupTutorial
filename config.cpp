7a0d255d3f4928d297dc23666cece20184253a8e
Thorsten Atzeni <dayz-zockerunion@gmx.de>
2021-10-27 23:42:37 +0200

    Lagschnitte - Added Clothing config.cpp

---
+++ config.cpp
@@ -0,0 +1,76 @@
+class CfgPatches
+{
+	class Your_ClassName
+	{
+		
+		units[]={};
+		weapons[]={};
+		requiredVersions=1.0;
+		requiredAddons[]=
+		{
+			"DZ_Characters",
+			"DZ_Characters_Backpacks",
+			"DZ_Characters_Pants",
+			"DZ_Characters_Tops",
+			"DZ_Gear_Containers",
+			"DZ_Characters_Headgear",
+			"DZ_Data"
+			
+		};
+	};
+};
+
+
+class CfgVehicles
+{
+	class Clothing;
+	class TShirt_Black;
+	class RadarCap_Black;
+	class Jeans_Black;
+	
+	
+	class Your_CLassname_Freshy_TShirt : TShirt_Black
+	{
+		displayName = "Your_CLassname_Freshy_TShirt";
+		descriptionShort = "Your_CLassname_Freshy_TShirt";
+		scope=2;
+		hiddenSelectionsTextures[]=
+		{
+			"Your_ClassName\data\tops\tshirt_black_co.paa",
+            "Your_ClassName\data\tops\tshirt_black_co.paa",
+            "Your_ClassName\data\tops\tshirt_black_co.paa"
+			
+		};
+		
+	};
+	
+	
+	
+	class LAG_Freshy_Jeans : Jeans_Black
+	{
+		displayName = "Your_CLassname_Freshy_TShirt";
+		descriptionShort = "Your_CLassname_Freshy_TShirt";
+		scope=2;
+		hiddenSelectionsTextures[]=
+		{
+			"Your_ClassName\data\pants\jeans_black_co.paa",
+            "Your_ClassName\data\pants\jeans_black_co.paa",
+            "Your_ClassName\data\pants\jeans_black_co.paa"
+		};
+		
+	};
+	
+	class LAG_Freshy_Radar_Cap : RadarCap_Black
+	{
+		displayName = "Your_CLassname_Freshy_TShirt";
+		descriptionShort = "Your_CLassname_Freshy_TShirt";
+		scope=2;
+		hiddenSelectionsTextures[]=
+		{
+			"Your_ClassName\data\headgear\radarcap_black_co.paa",
+            "Your_ClassName\data\headgear\radarcap_black_co.paa",
+            "Your_ClassName\data\headgear\radarcap_black_co.paa"
+		};
+		
+	};
+};
